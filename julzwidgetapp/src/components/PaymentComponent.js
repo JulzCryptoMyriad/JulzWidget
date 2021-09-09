1edfda0ee6eddcd7fa6d2e67de3c4b1173bf5125
JulissaDantes <julissa@singularsound.com>
2021-09-09 17:33:50 -0400

    Adding addresses

--- julzwidgetapp/src/components/PaymentComponent.js
+++ julzwidgetapp/src/components/PaymentComponent.js
@@ -76,9 +76,11 @@ export default class Payment extends Component {
                         onChange={this.onChangeToken.bind(this)}
                         >
                          <option value="0">Choose...</option>
-                            <option value="ETH">ETH</option>
-                            <option value="DAI">DAI</option>
-                            <option value="USDC">USDC</option>
+                         <option value="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2">ETH</option>
+                        <option value="0x6b175474e89094c44da98b954eedeac495271d0f">DAI</option>
+                        <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">USDC</option>
+                        <option value="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599">WBTC</option>
+                        <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">USDT</option>
                         </Form.Control>
                     </Col>
                 </Form.Group>
