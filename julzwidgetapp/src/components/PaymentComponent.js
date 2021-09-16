2559d999d21a7b0ab55e04d4474f16b013486879
JulissaDantes <julissa@singularsound.com>
2021-09-16 09:57:13 -0400

    Removing logs

--- julzwidgetapp/src/components/PaymentComponent.js
+++ julzwidgetapp/src/components/PaymentComponent.js
@@ -3,6 +3,7 @@ import '../App.css';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import {Card} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import {ethers} from 'ethers';
 import encodePath from '../services/encodePath.js'
@@ -46,14 +47,14 @@ export default class Payment extends Component {
             etherscan.getEtherPrice().then((price) => {
             this.setState({ethPrice: price});
             this.setState({itemPrice: Number(this.props.price.replace('?',''))/this.state.ethPrice});
-        });
+            });
         }catch(err){
             //todo use coingecko then
             console.log('Something went wrong with etherscan');
         }
 
         let data = await CoinGeckoClient.coins.fetch('ethereum', {});
-        console.log('what!',data.data.market_data.current_price.usd);
+        console.log(data.data.market_data.current_price.usd);
       }
       
      async onChangeToken(e) {
@@ -129,49 +130,54 @@ export default class Payment extends Component {
    render(){
            return (
         <div className="App">
-            Here you will pay
-            <Form>
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextInfo">
-                    <Form.Label column sm="5">
-                    Amount to be paid (US$)
-                    </Form.Label>
-                    <Col sm="5">
-                    <Form.Control plaintext readOnly defaultValue={this.props.price.replace('?','')} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formTokenSelect">
-                    <Form.Label column sm="5">
-                    Token to pay with
-                    </Form.Label>
-                    <Col xs="auto" className="5">
-                        <Form.Control
-                        as="select"
-                        custom
-                        onChange={this.onChangeToken.bind(this)}
-                        >
-                         <option value="0">Choose...</option>
-                         <option value="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2">ETH</option>
-                        <option value="0x6b175474e89094c44da98b954eedeac495271d0f">DAI</option>
-                        <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">USDC</option>
-                        <option value="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599">WBTC</option>
-                        <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">USDT</option>
-                        </Form.Control>
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formPlaintextInfo">
-                    <Form.Label column sm="5">
-                    Amount of selected token to be paid
-                    </Form.Label>
-                    <Col sm="5">
-                    <Form.Control plaintext readOnly value={this.state.itemPrice} />
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row} className="mb-3" controlId="formPayBtn">
-                    <Col xs="auto" className="my-1">
-                        <Button className="align-items-center" type="submit" disabled={ !this.state.value} onClick={this.onSubmit}>Pay</Button>    
-                    </Col>
-                </Form.Group>
-            </Form>
+            <Card>
+                <Card.Body>
+                    <Form>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextInfo">
+                            <Form.Label column sm="5">
+                            Amount to be paid (US$)
+                            </Form.Label>
+                            <Col sm="5">
+                            <Form.Control plaintext readOnly defaultValue={this.props.price.replace('?','')} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formTokenSelect">
+                            <Form.Label column sm="5">
+                            Token to pay with
+                            </Form.Label>
+                            <Col xs="auto" className="5">
+                                <Form.Control
+                                as="select"
+                                custom
+                                onChange={this.onChangeToken.bind(this)}
+                                >
+                                <option value="0">Choose...</option>
+                                <option value="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2">ETH</option>
+                                <option value="0x6b175474e89094c44da98b954eedeac495271d0f">DAI</option>
+                                <option value="0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48">USDC</option>
+                                <option value="0x2260fac5e5542a773aa44fbcfedf7c193bc2c599">WBTC</option>
+                                <option value="0xdac17f958d2ee523a2206206994597c13d831ec7">USDT</option>
+                                </Form.Control>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextInfo">
+                            <Form.Label column sm="5">
+                            Amount of selected token to be paid
+                            </Form.Label>
+                            <Col sm="5">
+                            <Form.Control plaintext readOnly value={this.state.itemPrice} />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formPayBtn">
+                            <Col sm="5"></Col>
+                            <Col xs="auto" >
+                                <Button className="align-items-center" type="submit" disabled={ !this.state.value} onClick={this.onSubmit}>Pay</Button>    
+                            </Col>
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+            </Card>
+           
         </div>
     )
    }
